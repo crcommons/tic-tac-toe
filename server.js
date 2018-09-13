e51c1e626bb1983858941ad57468d2977ac62a00
Carolyn Commons <crcommons@gmail.com>
2018-09-13 13:02:13 -0400

    resetting port for heroku

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const bodyParser     = require('body-parser');
 const app            = express();
 
 require('./app/routes')(app, {});
-const port = 8000;
-app.listen(port, () => {
-  console.log('We are live on ' + port);
+// const port = 8000;
+// app.listen(port, () => {
+//   console.log('We are live on ' + port);
+// });
+
+const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
+    console.log("App now running on port", port);
 });
